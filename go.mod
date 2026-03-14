5701c209f58e6d0453933b507fdf064866e7c4cb
vitalii <flatronsamatron@gmail.com>
2026-03-14 13:56:42 +0200

    add loger

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module go_api
 
 go 1.24.1
+
+require (
+	github.com/BurntSushi/toml v1.6.0 // indirect
+	github.com/sirupsen/logrus v1.9.4 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+)
--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -1,13 +1,39 @@
 package apiserver
 
-type Apiserver struct {
+import (
+	"github.com/sirupsen/logrus"
+)
 
+type Apiserver struct {
+	config *Config
+	logger *logrus.Logger
 }
 
-func New() *Apiserver{
-	return &Apiserver{}
+func New(config *Config) *Apiserver{
+	return &Apiserver{
+		config: config,
+		logger: logrus.New(),
+	}
 }
 
 func (s *Apiserver) Start() error{
+	if err := s.configureLogger(); err != nil {
+		return nil
+	}
+
+	s.logger.Info("starting api server")
+
+	return nil
+}
+
+func (s *Apiserver) configureLogger() error{
+	level, err := logrus.ParseLevel(s.config.LogLevel)
+
+	if err != nil {
+		return err
+	}
+
+	s.logger.SetLevel(level)
+
 	return nil
 }
